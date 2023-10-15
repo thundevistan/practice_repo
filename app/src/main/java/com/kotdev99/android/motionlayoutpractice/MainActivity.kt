2d93af5071c6a64232d1de4377d5f8216b4c446b
KotDev99 <KotDev99@gmail.com>
2023-10-16 00:23:06 +0900

    splash 효과 및 lottie 사용

--- app/src/main/java/com/kotdev99/android/motionlayoutpractice/MainActivity.kt
+++ app/src/main/java/com/kotdev99/android/motionlayoutpractice/MainActivity.kt
@@ -2,10 +2,31 @@ package com.kotdev99.android.motionlayoutpractice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
+
+	private lateinit var handler: Handler
+	private lateinit var logo: ImageView
+
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.activity_main)
+
+		handler = Handler(mainLooper)
+		logo = findViewById(R.id.logo)
+
+		handler.postDelayed({
+			kotlin.run {
+				logo.callOnClick()
+			}
+		}, 2000)
+
+		handler.postDelayed({
+			kotlin.run {
+				startActivity(DetailActivity.newIntent(this))
+			}
+		}, 3000)
 	}
 }
